bfcc59fd752d763344823ee0c7a11bded118088f
Rustem <rustem.karimov.2002@gmail.com>
2022-12-10 15:09:45 +0300

    init project

---
+++ src/main/java/ru/itis/hauntedo/simbirtest/SimbirTestApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.hauntedo.simbirtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimbirTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itis/hauntedo/simbirtest/SimbirTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.hauntedo.simbirtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbirTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
